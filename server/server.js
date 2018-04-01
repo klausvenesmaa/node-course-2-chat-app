9b4ea4c0ca82fced8ca7fb5fc9206a5fb84a1ffd
Venesmaa <klaus.venesmaa@accenture.com>
2018-04-01 11:30:00 +0300

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,20 +15,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'userOne',
-    text: 'Hellou maailma',
-    createdAt: new Date()
-  });
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
 
-  socket.on('createMessage', (newMessage) => {
-    console.log('createMessage', newMessage);
-    // socket.emit('newMessage', {
-    //   from: newMessage.from,
-    //   text: newMessage.text,
-    //   createdAt: new Date()
-    // });
-  })
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
 
   socket.on('disconnect', () => {
     console.log('User got disconnected');
